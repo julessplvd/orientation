8c9acd931ea8d1c64ce3853dc66f1e6a21f4efbe
Jules Sepulveda <julietrsepulveda@gmail.com>
2017-08-28 13:31:00 -0500

    Update 08.js

--- 08/08.js
+++ 08/08.js
@@ -1,7 +1,6 @@
 const { readFile } = require('fs');
 
-readFile(process.argv[2], (err, data
-) => {
+readFile(process.argv[2], (err, data) => {
     if (err) console.log(err);
     process.stdout.write(String(data));
 });
